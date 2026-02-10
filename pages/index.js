4867f71ed952330c461a748a7a42612fe7e41a57
Amberetu <ajamb923@gmail.com>
2026-02-10 06:34:36 -0500

    initial commit
    Signed-off-by: Amberetu <ajamb923@gmail.com>

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial, sans-serif", padding: 40 }}>
+      <h1>Hello World 👋</h1>
+      <p>Next.js running on ECS Fargate.</p>
+    </main>
+  );
+}
