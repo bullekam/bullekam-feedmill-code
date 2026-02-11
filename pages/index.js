2b65792ec42a1c0384c15d334f3ccdbada3dd645
Amberetu <ajamb923@gmail.com>
2026-02-11 18:41:11 -0500

    updates & automation
    Signed-off-by: Amberetu <ajamb923@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ fontFamily: "Arial, sans-serif", padding: 40 }}>
       <h1>Hello World 👋</h1>
-      <p>Next.js running on ECS Fargate.</p>
+      <p>Next.js running on ECS Fargate updated.</p>
     </main>
   );
 }
